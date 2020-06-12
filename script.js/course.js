4275d22aba646b575ae12a45723845a991b3afbe
Bogdan <bogdan.malyshev.art@gmail.com>
2020-06-12 22:30:17 +0200

    New Commit

--- script.js/course.js
+++ script.js/course.js
@@ -7,26 +7,27 @@ let options = {
 	colors:{
 		border:"green",
 		bg:"black"
+	},
+	nameTest: function(){
+		const basicMassiv = [];
+		let counter = 1;
+		for (let key in options) {
+			if (typeof (options[key])  === "object") {
+				for (let i in options[key]){
+				console.log(counter);
+				console.log(`Ключ под названием ${i} имеет значение ${options[key][i]}`);
+				basicMassiv[counter-1] = i;
+				counter++;}
+			}else if (key === "nameTest"){
+				
+			}else{
+				console.log(counter);
+				console.log(`Ключ под названием ${key} имеет значение ${options[key]}`);
+				basicMassiv[counter-1] = key;
+				counter++;
+			}
+		}
+		console.log(basicMassiv);
 	}
 }
-const basicMassiv = [];
-let counter = 1;
-
-for (let key in options) {
-	if (typeof (options[key])  === "object") {
-		for (let i in options[key]){
-
-			console.log(counter);
-			console.log(`Ключ под названием ${i} имеет значение ${options[key][i]}`);
-			basicMassiv[counter-1] = i;
-			counter++;}
-	}else{
-		console.log(counter);
-		console.log(`Ключ под названием ${key} имеет значение ${options[key]}`);
-		basicMassiv[counter-1] = key;
-		counter++;
-
-	}
-}
-
-console.log(basicMassiv);
+options.nameTest();
