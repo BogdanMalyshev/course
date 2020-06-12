db8eaba2badcc2ef5a9ff039d5ad46f73bf4bbe8
Bogdan <bogdan.malyshev.art@gmail.com>
2020-06-12 21:44:29 +0200

    New commit

--- script.js/course.js
+++ script.js/course.js
@@ -9,13 +9,24 @@ let options = {
 		bg:"black"
 	}
 }
+const basicMassiv = [];
+let counter = 1;
 
 for (let key in options) {
 	if (typeof (options[key])  === "object") {
-		for (let i in options[key]) {
+		for (let i in options[key]){
+
+			console.log(counter);
 			console.log(`Ключ под названием ${i} имеет значение ${options[key][i]}`);
-		}
+			basicMassiv[counter-1] = i;
+			counter++;}
 	}else{
+		console.log(counter);
 		console.log(`Ключ под названием ${key} имеет значение ${options[key]}`);
+		basicMassiv[counter-1] = key;
+		counter++;
+
 	}
 }
+
+console.log(basicMassiv);
